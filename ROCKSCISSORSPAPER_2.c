c2bc7196a588b2a5249438446df750f3cad933fb
Menboong <ruby020415@naver.com>
2018-07-10 19:14:38 +0900

    correct_1
    I add error detector

--- ROCKSCISSORSPAPER_2.c
+++ ROCKSCISSORSPAPER_2.c
@@ -3,7 +3,15 @@ main()
 {
 	int me,com;
 	
-	scanf("%d %d",&me,&com);
+	while(1)
+	{
+		scanf("%d %d",&me,&com);
+		if(me<1 || me>3 || com<1 || com>3)
+		{
+			printf("ERROR\n");
+		}
+		else break;
+	}
 	
 	if(me==0 && com ==1 || me==1 && com ==2 || me==2&&com==0)
 	{
