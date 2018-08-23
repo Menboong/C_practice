67d122eb4769f5d180d8866a21374b50320634d1
Menboong <ruby020415@naver.com>
2018-08-23 19:53:45 +0900

    클릭게임 ver.2

--- getch응용한 클릭게임.c
+++ getch응용한 클릭게임.c
@@ -16,6 +16,7 @@ main()
 		{
 			printf("■");
 		}
+		printf("\n%d\n",cnt);
 		printf("\n키보드를 누르면 아파여...\n----------\n|        |\n|  |  |  |\n|   ㅅ   |\n----------\n");
 		getch();
 		system("cls");
@@ -23,85 +24,79 @@ main()
 		Sleep(500);
 		system("cls");
 		cnt++;
-		
-		if(cnt == 20)
+
+		if(cnt == 20) n++;
+		else if(cnt == 40) n++;
+		else if(cnt == 60) n++;
+		else if(cnt == 80) n++;
+		else if(cnt == 100) n++;
+		else if(cnt == 101) n++;
+
+		switch (n)
 		{
-			printf("심심이의 분노게이지가 한칸 올라갑니다.\n");
-			printf("----------\n|        |\n| ㅜ  ㅜ |\n|   ㅅ   |\n----------\n끄앙..\n");
-			printf("아무키나 눌러주세요.\n");
-			getch();
-			system("cls");
-			n++;
-		}
-		else if(cnt==40)
-		{
-			
-			printf("심심이의 분노게이지가 한칸 올라갑니다.\n");
-			printf("----------\n|        |\n| ㅡ  ㅡ |\n|   ㅅ   |\n----------\n힝..구\n");
-			printf("아무키나 눌러주세요.\n");
-			getch();
-			system("cls");
-			n++;
-		}
-		else if(cnt ==60)
-		{
-			
-			printf("심심이의 분노게이지가 한칸 올라갑니다.\n");
-			printf("----------\n|        |\n| ㅡ  ㅡ |\n|   ㅡ   |\n----------\n그럴꺼야??\n");
-			printf("아무키나 눌러주세요.\n");
-			getch();
-			system("cls");
-			n++;
-		}
-		else if(cnt ==80)
-		{
-			
-			printf("심심이의 분노게이지가 한칸 올라갑니다.\n");
-			printf("----------\n|        |\n|  \  /  |\n|   ㅡ   |\n----------\n짜증나!\n");
-			printf("아무키나 눌러주세요.\n");
-			getch();
-			system("cls");
-			n++;
-		}
-		else if(cnt ==100)
-		{
-			
-			printf("심심이의 분노게이지가 한칸 올라갑니다.\n");
-			printf("----------\n|        |\n|  \  /  |\n|   o   |\n----------\n하지마!!!\n");
-			printf("아무키나 눌러주세요.\n");
-			getch();
-			system("cls");
-			n++;
-		}
-		else if(cnt >=101)
-		{
-			
-			printf("심심이의 분노게이지가 한칸 올라갑니다.\n");
-			printf("----------\n|        |\n|  \  /  |\n|   o   |\n----------\n하지말라고 했지?!\n");
-			printf("5초 뒤 심심이가 폭주합니다.\n");
-			Sleep(5000);
-			for(i=0;i<1000;i++)
-			{
-				printf("하지마!\n");
-			}
-			printf("지금 지켜만 보고 있는거야?\n");
-			printf("이런거 하면 되요? 안되요? Y/N(대문자로 입력해주세요)");
-			scanf("%c",&key);
-			if(key == 'Y')
-			{
-				printf("앞으로는 그러지마여!!");
+			case 1 :
+				printf("심심이의 분노게이지가 한칸 올라갑니다.\n");
+				printf("----------\n|        |\n| ㅜ  ㅜ |\n|   ㅅ   |\n----------\n끄앙..\n");
+				Sleep(2000);
+				printf("아무키나 눌러주세요.\n");
+				getch();
+				system("cls");
 				break;
-			}
-			else if(key == 'N')
-			{
-				printf("헐..\n");
-				printf("심심이가 삐졌습니다.\n");
+			case 2 :
+				printf("심심이의 분노게이지가 한칸 올라갑니다.\n");
+				printf("----------\n|        |\n| ㅡ  ㅡ |\n|   ㅅ   |\n----------\n힝..구\n");
+				Sleep(2000);
+				printf("아무키나 눌러주세요.\n");
+				getch();
+				system("cls");
+				break;
+			case 3 :
+				printf("심심이의 분노게이지가 한칸 올라갑니다.\n");
+				printf("----------\n|        |\n| ㅡ  ㅡ |\n|   ㅡ   |\n----------\n그럴꺼야??\n");
+				Sleep(2000);
+				printf("아무키나 눌러주세요.\n");
+				getch();
+				system("cls");
+				break;
+			case 4 :
+				printf("심심이의 분노게이지가 한칸 올라갑니다.\n");
+				printf("----------\n|        |\n|  \  /  |\n|   ㅡ   |\n----------\n짜증나!\n");
+				Sleep(2000);
+				printf("아무키나 눌러주세요.\n");
+				getch();
+				system("cls");
+				break;
+			case 5 :
+				printf("심심이의 분노게이지가 한칸 올라갑니다.\n");
+				printf("----------\n|        |\n|  \  /  |\n|   o   |\n----------\n하지마!!!\n");
+				printf("아무키나 눌러주세요.\n");
+				getch();
+				system("cls");
+				break;
+			case 6 :
+				printf("심심이의 분노게이지가 한칸 올라갑니다.\n");
+				printf("----------\n|        |\n|  \  /  |\n|   o   |\n----------\n하지말라고 했지?!\n");
+				printf("5초 뒤 심심이가 폭주합니다.\n");
+				Sleep(5000);
 				for(i=0;i<1000;i++)
 				{
-					printf("저리가!\n");
+					printf("하지마!\n");
 				}
-				break; 
-			}
-		}
+				printf("지금 지켜만 보고 있는거야?\n");
+				printf("이런거 하면 되요? 안되요? Y/N(대문자로 입력해주세요)");
+				scanf("%c",&key);
+				if(key == 'Y') printf("앞으로는 그러지마여!!");
+				else if(key == 'N')
+				{
+					printf("헐..\n");
+					printf("심심이가 삐졌습니다.\n");
+					for(i=0;i<1000;i++)
+					{
+						printf("저리가!\n");
+					}
+				}
+				break;
+		}			
+		if(cnt >=101) break;
 	}
 }
