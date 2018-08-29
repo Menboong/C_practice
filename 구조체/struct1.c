48f5a5b2d2a717c343a91f3436cd892b7bb86fd0
Menboong <ruby020415@naver.com>
2018-08-29 11:07:04 +0900

    struct_practice

---
+++ 구조체/struct1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h> 
+
+struct Person
+{
+	char name [20];
+	int age;
+	char address[100];
+};
+
+int main()
+{
+	struct Person p1;
+	
+	strcpy(p1.name,"최소영");
+	p1.age = 17;
+	strcpy(p1.address, "콜라 맘속 >_<");
+	
+	printf("이름: %s\n",p1.name);
+	printf("나이: %d\n",p1.age);
+	printf("주소: %s\n",p1.address); 
+}
+
