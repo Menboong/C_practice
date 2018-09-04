b23eb44a1ba80258fc47f8871d52bb252f1a6a17
Menboong <ruby020415@naver.com>
2018-09-04 23:19:26 +0900

    제곱함수

---
+++ pointer/num제곱함수.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+//call by-value
+/*int mul(int num)
+{
+	return num*num;
+}
+
+void main()
+{
+	int num;
+	scanf("%d",&num);
+	printf("%d",mul(num));
+}*/
+
+//call by-reference
+/*int mul(int *num)
+{
+	int num2 = (*num)*(*num);
+	return num2;
+}
+
+void main()
+{
+	int num;
+	scanf("%d",&num);
+	printf("%d",mul(&num));
+}*/
