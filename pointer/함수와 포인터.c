7081d97fc7b7c7f8fd7edacc53a9abb919defa1c
Menboong <ruby020415@naver.com>
2018-09-04 23:19:40 +0900

    함수와포인터

---
+++ pointer/함수와 포인터.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+// call by-value 
+/*void Swap(int n1,int n2)
+{
+	int temp = n1;
+	n1 = n2;
+	n2 = temp;
+	printf("%d %d\n",n1,n2);
+}
+
+void main()
+{
+	int num1 = 10;
+	int num2 = 20;
+	printf("%d %d\n",num1,num2);
+	Swap(num1,num2);
+	printf("%d %d\n",num1.num2);
+}*/
+
+//call by-reference
+/*void Swap(int *n1,int *n2)
+{
+	int temp = *n1;
+	*n1 = *n2;
+	*n2 = temp;
+	printf("%d %d\n",*n1,*n2);
+}
+
+void main()
+{
+	int num1 = 10;
+	int num2 = 20;
+	printf("%d %d\n",num1,num2);
+	Swap(&num1,&num2);
+	printf("%d %d\n",num1,num2);
+}*/
+
+
